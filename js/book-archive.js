8dc3b40bb5aedd3447309d6d89db6c8031055c92
AyeshaAktar <ayeshaaktar491@gmail.com>
2021-09-02 14:34:24 +0600

    cover added

--- js/book-archive.js
+++ js/book-archive.js
@@ -1,38 +1,48 @@
 const searchBook = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    // console.log(searchText);
-    searchField.value = '';
-    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  searchField.value = "";
+  const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
-    // console.log(url);
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displaySearchResult(data.docs));
-}
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displaySearchResult(data.docs);
 
-const displaySearchResult = docs => {
-    const searchResult = document.getElementById('search-result');
-    docs.forEach(doc => {
-        console.log(doc);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+      if (searchText === "") {
+        alert("Please write something");
+      } else {
+        if (data.docs.length > 0) {
+          document.getElementById(
+            "error-message"
+          ).innerHTML = `Total-Result: ${data.num_found}`;
+        } else {
+          document.getElementById("error-message").innerHTML =
+            "No Result Found";
+        }
+      }
+    });
+};
+
+const displaySearchResult = (docs) => {
+  const searchResult = document.getElementById("search-result");
+  searchResult.textContent = "";
+  docs.forEach((doc) => {
+    const div = document.createElement("div");
+    // console.log(doc);
+    const imageSource = `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg`;
+    div.classList.add("col");
+    div.innerHTML = `
         <div class="card h-100">
-          <!-- <img src="..." class="card-img-top" alt="..." /> -->
+          <img src=${imageSource} class="card-img-top" style='height:300px' alt="..." />
           <div class="card-body">
-            <h4 class="card-title">${doc.title}</h4>
-            <h6 class="card-title">${doc.author_name}</h6>
-            <h6 class="card-title">${doc.publisher}</h6>
-            <h6 class="card-title">${doc.first_publish_year}</h6>
-            // <p class="card-text">
-            //   This is a longer card with supporting text below as a natural
-            //   lead-in to additional content. This content is a little bit
-            //   longer.
-            // </p>
+            <h4 class="card-title text-success">book name: ${doc.title}</h4>
+            <h6 class="card-title">author name: ${doc.author_name}</h6>
+            <h6 class="card-title">publisher: ${doc.publisher}</h6>
+            <h6 class="card-title">1st published: ${doc.first_publish_year}</h6>
           </div>
         </div>
         `;
-        searchResult.appendChild(div);
-    })
-}
+    searchResult.appendChild(div);
+  });
+};
